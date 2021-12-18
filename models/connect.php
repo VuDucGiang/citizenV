878e8b190051cd2d442544a8baa66608fa7f8ed1
19020352 <61815290+19020352@users.noreply.github.com>
2021-12-18 08:13:09 +0700

    đã submit đc vào csdl
    đã submit đc vào csdl

--- models/connect.php
+++ models/connect.php
@@ -3,7 +3,7 @@
         $host = "localhost";   
         $dbname = "citizenv"; 
         $username = "root";    
-        $password = "";        
+        $password = "123";        
         //$password = "hiep";
         $this->pdo = new PDO("mysql:host=$host; dbname=$dbname;", $username, $password);
     } catch (PDOException $e) {
