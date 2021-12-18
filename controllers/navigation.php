cf66eb735c3a74606598744662cc7f59d0d4e54f
hippyhoe <hippyhoe@users.noreply.github.com>
2021-12-18 15:13:34 +0700

    thêm phần thống kê

--- controllers/navigation.php
+++ controllers/navigation.php
@@ -1,40 +1,26 @@
 <?php
-  if ((!isset($_GET['page']))) include 'infoController.php';
+  if ((!isset($_GET['page']))) include 'registrationController.php';
 
   if(isset($_GET['page'])) {
     $page=$_GET['page'];
 
     if ($page == '1'){
+
       include 'registrationController.php';
     }
     if ($page == '2'){
       include 'infoController.php';
     }
     if ($page == '3'){
+      include 'statisticsController.php';
+    }
+    if ($page == '4'){
+      include 'manageController.php';
+    }
+    if ($page == '5'){
       session_unset(); 
       session_destroy();
       header('Location: ../index.php');
     }
-    if ($page == '4'){
-      include 'changePassController.php';
-    }
-    if ($page == '5'){
-      include 'manageController.php';
-    }
-    if ($page == '6'){
-      include 'statisticsController.php';
-    }
-    // if ($page == '31'){
-    //   include 'admin/marks_view.php';
-    // }
-    // if ($page == '4'){
-    //   include 'pages/login.php';
-    // }
-
-    // if ($page == '6'){
-    //   session_unset(); 
-    //   session_destroy();
-    //   header('Location: index.php');
-    // }
   }
 ?>
