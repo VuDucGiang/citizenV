2134d2a19f177b8800bf39240eaa711ddb10e571
VuDucGiang <ducgiang.nd1808@gmail.com>
2021-12-23 13:12:46 +0700

    done kiem tra hop thuc

--- models/registrationModel.php
+++ models/registrationModel.php
@@ -10,10 +10,11 @@
         public function submit() {
 
             $f_cccd=$f_name=$b_date=$gender=$que_tinh=$que_huyen=$que_xa=$que_duong=$thuong_tinh=$thuong_huyen=$thuong_xa=$thuong_duong
-            =$tam_tinh=$tam_huyen=$tam_xa=$tam_duong=$van_hoa=$ton_giao=$job=$ma_vung="";
-            $error_msg="";
+            =$tam_tinh=$tam_huyen=$tam_xa=$tam_duong=$van_hoa=$ton_giao=$job=$ma_vung=NULL;
+            $error_msg=NULL;
             $que_quan = $thuong_tru= $tam_tru="";
 
+        
             if(isset($_POST['submit_but'])) 
             {
             try {   
@@ -35,27 +36,37 @@
                 $ton_giao = $_POST['ton_giao'];
                 $job = $_POST['job'];
                 $ma_vung = $_POST['ma_vung'];
-                
-                $stmt = $this -> pdo ->prepare("INSERT INTO `info` (`cccd`, `ten`, `ngaySinh`, `gioiTinh`, `queQuan`, `diaChiThuongTru`, `diaChiTamTru`, `tonGiao`, `trinhDoVanHoa`, `ngheNghiep`, `maDiaChi`) 
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
 
-               
-                $stmt -> bindValue(1, $f_cccd);
-                $stmt -> bindValue(2, $f_name);
-                $stmt -> bindValue(3, $b_date);
-                $stmt -> bindValue(4, $gender);
-                $stmt -> bindValue(5, $que_quan);
-                $stmt -> bindValue(6, $thuong_tru);
-                $stmt -> bindValue(7, $tam_tru);
-                $stmt -> bindValue(8, $ton_giao);
-                $stmt -> bindValue(9, $van_hoa);
-                $stmt -> bindValue(10, $job);
-                $stmt -> bindValue(11, $ma_vung);
+                $lenCccd = strlen($f_cccd);
+
+                // Kiểm tra hợp thức trước khi insert date lên server
+                if($lenCccd == 9 || $lenCccd == 12 && isset($f_name) && isset($b_date) && isset($gender) &&
+                isset($que_quan) && isset($thuong_tru) && isset($tam_tru) && isset($van_hoa) && isset($ma_vung)) {
+
+                    $stmt = $this -> pdo ->prepare("INSERT INTO `info` (`cccd`, `ten`, `ngaySinh`, `gioiTinh`, `queQuan`, `diaChiThuongTru`, `diaChiTamTru`, `tonGiao`, `trinhDoVanHoa`, `ngheNghiep`, `maDiaChi`) 
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+
                 
-                $stmt -> execute();
-                
-                $message = "Nhập dữ liệu thành công!";
-                echo "<script type='text/javascript'>alert('$message');</script>";
+                    $stmt -> bindValue(1, $f_cccd);
+                    $stmt -> bindValue(2, $f_name);
+                    $stmt -> bindValue(3, $b_date);
+                    $stmt -> bindValue(4, $gender);
+                    $stmt -> bindValue(5, $que_quan);
+                    $stmt -> bindValue(6, $thuong_tru);
+                    $stmt -> bindValue(7, $tam_tru);
+                    $stmt -> bindValue(8, $ton_giao);
+                    $stmt -> bindValue(9, $van_hoa);
+                    $stmt -> bindValue(10, $job);
+                    $stmt -> bindValue(11, $ma_vung);
+                    
+                    $stmt -> execute();
+                    
+                    $message = "Nhập dữ liệu thành công!";
+                    echo "<script type='text/javascript'>alert('$message');</script>";
+                } /*else {
+                    $message = "Nhập dữ liệu thất bại!";
+                    echo "<script type='text/javascript'>alert('$message');</script>";
+                }*/
 
             }
             
